167df5db0efb957cba77e8d871c08afc86a63032
mohammed Rashid <developmenta72@gmail.com>
2025-08-07 09:06:39 +0800

    Add files via upload

---
+++ big-o-visualizer/src/App.tsx
@@ -0,0 +1,11 @@
+import BigOVisualization from './components/BigOVisualization';
+
+function App() {
+  return (
+    <div className="App">
+      <BigOVisualization />
+    </div>
+  );
+}
+
+export default App;
